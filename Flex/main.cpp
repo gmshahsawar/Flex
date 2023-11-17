672e02e9aa980d1ad7ed0f5a3f7d0257a747ff62
Ghulam Muhammad <l226938@lhr.nu.edu.pk>
2023-11-17 22:14:49 +0500

    Initial Commit

---
+++ Flex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Flex
+//
+//  Created by MAC on 17/11/2023.
+//  Copyright © 2023 MAC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
