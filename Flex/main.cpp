5d2076fa9aa24823af4b19716469592cdd0254b3
Ghulam Muhammad <l226938@lhr.nu.edu.pk>
2023-11-19 18:00:08 +0500

    class student header file

--- Flex/main.cpp
+++ Flex/main.cpp
@@ -1,15 +1,8 @@
-//
-//  main.cpp
-//  Flex
-//
-//  Created by MAC on 17/11/2023.
-//  Copyright © 2023 MAC. All rights reserved.
-//
-
 #include <iostream>
+#include"student.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(){
+    
+    cout<<a<<endl;
 }
